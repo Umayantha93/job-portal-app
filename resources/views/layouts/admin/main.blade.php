4dcfb714689002e749a951c94cf58f2db54dbaee
Umayantha93 <umayantha93@gmail.com>
2024-11-29 07:22:32 +0530

    integrate admin template

---
+++ resources/views/layouts/admin/main.blade.php
@@ -0,0 +1,22 @@
+@include('layouts.admin.header')
+
+<body class="sb-nav-fixed">
+
+    @include('layouts.admin.navbar')
+
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+
+            @include('layouts.admin.sidebar')
+
+        </div>
+        <div id="layoutSidenav_content">
+            {{-- @include('layouts.admin.body') --}}
+            @yield('content')
+            @include('layouts.admin.footer')
+        </div>
+    </div>
+
+</body>
+
+</html>
