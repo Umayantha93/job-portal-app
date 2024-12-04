9be891cd6eeb73574ed27b513c833a980b0db5cb
Umayantha93 <umayantha93@gmail.com>
2024-12-04 15:53:32 +0530

    make a migration file to post a job

---
+++ resources/views/job/create.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin.main')
+
+@section('content')
+
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            form
+        </div>
+    </div>
+
+@endsection
